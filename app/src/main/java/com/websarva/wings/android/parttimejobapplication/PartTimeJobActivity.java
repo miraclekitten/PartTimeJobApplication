83d844be8bd5049c1fe3fc03d1395fc4116e755c
miraclekitten <miraclekitten@users.noreply.github.com>
2019-06-13 17:31:56 +0900

    1.インテントで渡したい数値をフィールド化

--- app/src/main/java/com/websarva/wings/android/parttimejobapplication/PartTimeJobActivity.java
+++ app/src/main/java/com/websarva/wings/android/parttimejobapplication/PartTimeJobActivity.java
@@ -39,25 +39,44 @@ public class PartTimeJobActivity extends AppCompatActivity {
 
     private class BtnListener implements View.OnClickListener{
 
+        private EditText dailyEdit = findViewById(R.id.daily_edittext);
+        private EditText daily2Edit = findViewById(R.id.daily2_edittext);
+        private EditText dailyPlusEdit = findViewById(R.id.dailyPlus_edittext);
+        private EditText dailyPlus2Edit = findViewById(R.id.dailyPlus2_edittext);
+        private EditText sdEdit = findViewById(R.id.sdSet_edittext);
+        private EditText sd2Edit = findViewById(R.id.sd2_edittext);
+        private EditText karuEdit = findViewById(R.id.karubi_edittext);
+        private EditText karu2Edit = findViewById(R.id.karubi2_edittext);
+        private EditText gohanBigEdit = (EditText) findViewById(R.id.gohanBig_edittext);
+        private EditText gohanSmallEdit = (EditText) findViewById(R.id.gohanSmall_edittext);
+        private TextView menu1 = findViewById(R.id.menu1);
+        private TextView menu2 = findViewById(R.id.menu2);
+        private TextView menu3 = findViewById(R.id.menu3);
+        private TextView menu4 = findViewById(R.id.menu4);
+        private TextView gohan = findViewById(R.id.gohan);
+        private TextView gohanSub = findViewById(R.id.gohanSub);
+
+        private int dailyNum = 0;
+        private int dailyBox = 0;
+        private int dailyRem = 0;
+        private int dailyPlusNum = 0;
+        private int dailyPlusBox = 0;
+        private int dailyPlusRem = 0;
+        private int sdNum = 0;
+        private int sdBox = 0;
+        private int sdRem = 0;
+        private int karuNum = 0;
+        private int karuBox = 0;
+        private int karuRem = 0;
+        private int gohanBox = 0;
+        private int gohanRem = 0;
+        private int gohanBigNum = 0;
+        private int gohanSmallNum =  0;
+
         @Override
         public void onClick(View view){
             int id = view.getId();
-            EditText dailyEdit = findViewById(R.id.daily_edittext);
-            EditText daily2Edit = findViewById(R.id.daily2_edittext);
-            EditText dailyPlusEdit = findViewById(R.id.dailyPlus_edittext);
-            EditText dailyPlus2Edit = findViewById(R.id.dailyPlus2_edittext);
-            EditText sdEdit = findViewById(R.id.sdSet_edittext);
-            EditText sd2Edit = findViewById(R.id.sd2_edittext);
-            EditText karuEdit = findViewById(R.id.karubi_edittext);
-            EditText karu2Edit = findViewById(R.id.karubi2_edittext);
-            EditText gohanBigEdit = (EditText) findViewById(R.id.gohanBig_edittext);
-            EditText gohanSmallEdit = (EditText) findViewById(R.id.gohanSmall_edittext);
-            TextView menu1 = findViewById(R.id.menu1);
-            TextView menu2 = findViewById(R.id.menu2);
-            TextView menu3 = findViewById(R.id.menu3);
-            TextView menu4 = findViewById(R.id.menu4);
-            TextView gohan = findViewById(R.id.gohan);
-            TextView gohanSub = findViewById(R.id.gohanSub);
+
 
             switch (id) {
 
@@ -78,9 +97,9 @@ public class PartTimeJobActivity extends AppCompatActivity {
                     デイリーを計算する処理
                      */
                     if(Integer.parseInt(dailyEdit.getText().toString()) != 0 && Integer.parseInt(daily2Edit.getText().toString()) != 0) {
-                        int dailyNum = Integer.parseInt(dailyEdit.getText().toString()) + Integer.parseInt(daily2Edit.getText().toString());
-                        int dailyBox = dailyNum / 8;
-                        int dailyRem = dailyNum % 8;
+                        dailyNum = Integer.parseInt(dailyEdit.getText().toString()) + Integer.parseInt(daily2Edit.getText().toString());
+                        dailyBox = dailyNum / 8;
+                        dailyRem = dailyNum % 8;
                         String dailyStr = "デイリーの合計数は " + dailyNum + " です。(箱" + dailyBox + "個と" + dailyRem + "個です)";
                         dailyStr = dailyStr.replace(String.valueOf(dailyNum) + " です", "<strong><font color=\"blue\">" + String.valueOf(dailyNum) + "</font></strong> です");
                         dailyStr = dailyStr.replace("箱" + String.valueOf(dailyBox) + "個", "箱<strong><font color=\"red\">" + String.valueOf(dailyBox) + "</font></strong>個");
@@ -100,9 +119,9 @@ public class PartTimeJobActivity extends AppCompatActivity {
                     デイリープラスを計算する処理
                      */
                     if(Integer.parseInt(dailyPlusEdit.getText().toString()) != 0 && Integer.parseInt(dailyPlus2Edit.getText().toString()) != 0) {
-                        int dailyPlusNum = Integer.parseInt(dailyPlusEdit.getText().toString()) + Integer.parseInt(dailyPlus2Edit.getText().toString());
-                        int dailyPlusBox = dailyPlusNum / 6;
-                        int dailyPlusRem = dailyPlusNum % 6;
+                        dailyPlusNum = Integer.parseInt(dailyPlusEdit.getText().toString()) + Integer.parseInt(dailyPlus2Edit.getText().toString());
+                        dailyPlusBox = dailyPlusNum / 6;
+                        dailyPlusRem = dailyPlusNum % 6;
                         String dailyPlusStr = "デイリープラスの合計数は " + dailyPlusNum + " です。(箱" + dailyPlusBox + "個と" + dailyPlusRem + "個です)";
                         dailyPlusStr = dailyPlusStr.replace(String.valueOf(dailyPlusNum) + " です", "<strong><font color=\"blue\">" + String.valueOf(dailyPlusNum) + "</font></strong> です");
                         dailyPlusStr = dailyPlusStr.replace("箱" + String.valueOf(dailyPlusBox) + "個", "箱<strong><font color=\"red\">" + String.valueOf(dailyPlusBox) + "</font></strong>個");
@@ -122,9 +141,9 @@ public class PartTimeJobActivity extends AppCompatActivity {
                     無ければ何もしない
                      */
                     if(Integer.parseInt(sdEdit.getText().toString()) != 0 || Integer.parseInt(sd2Edit.getText().toString()) != 0) {
-                        int sdNum = Integer.parseInt(sdEdit.getText().toString()) + Integer.parseInt(sd2Edit.getText().toString());
-                        int sdBox = sdNum / 6;
-                        int sdRem = sdNum % 6;
+                        sdNum = Integer.parseInt(sdEdit.getText().toString()) + Integer.parseInt(sd2Edit.getText().toString());
+                        sdBox = sdNum / 6;
+                        sdRem = sdNum % 6;
                         String sdStr = "SDの合計数は " + sdNum + " です。(箱" + sdBox + "個と" + sdRem + "個です)";
                         sdStr = sdStr.replace(String.valueOf(sdNum) + " です", "<strong><font color=\"blue\">" + String.valueOf(sdNum) + "</font></strong> です");
                         sdStr = sdStr.replace("箱" + String.valueOf(sdBox) + "個", "箱<strong><font color=\"red\">" + String.valueOf(sdBox) + "</font></strong>個");
@@ -141,9 +160,9 @@ public class PartTimeJobActivity extends AppCompatActivity {
                     無ければ何もしない
                      */
                     if(Integer.parseInt(karuEdit.getText().toString()) != 0 || Integer.parseInt(karu2Edit.getText().toString()) != 0) {
-                        int karuNum = Integer.parseInt(karuEdit.getText().toString()) + Integer.parseInt(karu2Edit.getText().toString());
-                        int karuBox = karuNum / 6;
-                        int karuRem = karuNum % 6;
+                        karuNum = Integer.parseInt(karuEdit.getText().toString()) + Integer.parseInt(karu2Edit.getText().toString());
+                        karuBox = karuNum / 6;
+                        karuRem = karuNum % 6;
                         String karuStr = "カルビの合計数は " + karuNum + " です。(箱" + karuBox + "個と" + karuRem + "個です)";
                         karuStr = karuStr.replace(String.valueOf(karuNum) + " です", "<strong><font color=\"blue\">" + String.valueOf(karuNum) + "</font></strong> です");
                         karuStr = karuStr.replace("箱" + String.valueOf(karuBox) + "個", "箱<strong><font color=\"red\">" + String.valueOf(karuBox) + "</font></strong>個");
@@ -160,10 +179,10 @@ public class PartTimeJobActivity extends AppCompatActivity {
                      *　各場所で足し合わせているご飯の数から計算する
                      */
                     if(gohanNum != 0){
-                        int gohanBox = gohanNum / 20;
-                        int gohanRem = gohanNum % 20;
-                        int gohanBigNum = Integer.parseInt(gohanBigEdit.getText().toString());
-                        int gohanSmallNum =  Integer.parseInt(gohanSmallEdit.getText().toString());
+                        gohanBox = gohanNum / 20;
+                        gohanRem = gohanNum % 20;
+                        gohanBigNum = Integer.parseInt(gohanBigEdit.getText().toString());
+                        gohanSmallNum =  Integer.parseInt(gohanSmallEdit.getText().toString());
                         String gohanStr = "ごはんの合計数は " + gohanNum + " です。(箱" + gohanBox + "個と" + gohanRem + "個です)";
                         String gohanSubStr = "普通盛は" + (gohanNum - (gohanBigNum + gohanSmallNum)) + "個で、大盛は" + gohanBigNum + "個で、小盛は" + gohanSmallNum + "個です。";
 
@@ -184,6 +203,10 @@ public class PartTimeJobActivity extends AppCompatActivity {
                 case R.id.btClear:
                     clearEditText(dailyEdit, daily2Edit, dailyPlusEdit, dailyPlus2Edit, sdEdit, sd2Edit, karuEdit, karu2Edit);
                     clearMenu(menu1, menu2, menu3, menu4, gohan, gohanSub);
+                    break;
+
+                case R.id.btPrepareNext:
+
                     break;
 
             }
