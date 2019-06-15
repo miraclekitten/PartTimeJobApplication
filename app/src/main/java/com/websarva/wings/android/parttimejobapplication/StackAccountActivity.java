eeec8307e108b6a7adf98e3d43c856b3962676ac
miraclekitten <miraclekitten@users.noreply.github.com>
2019-06-16 00:42:02 +0900

    1.位置情報取得処理の追加 2.データベース接続処理途中まで作成

--- app/src/main/java/com/websarva/wings/android/parttimejobapplication/StackAccountActivity.java
+++ app/src/main/java/com/websarva/wings/android/parttimejobapplication/StackAccountActivity.java
@@ -1,13 +1,139 @@
 package com.websarva.wings.android.parttimejobapplication;
 
+import android.Manifest;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+import android.location.Location;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class StackAccountActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stack_account);
+
+        // LocationClientクラスのインスタンスを生成
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        Button saveBtn = findViewById(R.id.saveAccount_btn);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // 位置情報取得開始
+                startUpdateLocation();
+            }
+        });
+
     }
+
+    /**
+     * 位置情報取得開始メソッド
+     */
+    private void startUpdateLocation() {
+        // 位置情報取得権限の確認
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 権限がない場合、許可ダイアログ表示
+            String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
+            ActivityCompat.requestPermissions(this, permissions, 2000);
+            return;
+        }
+
+        // 位置情報の取得方法を設定
+        LocationRequest locationRequest = LocationRequest.create();
+ //       locationRequest.setInterval(10000);       // 位置情報更新間隔の希望
+ //       locationRequest.setFastestInterval(5000); // 位置情報更新間隔の最速値
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); // この位置情報要求の優先度
+
+        fusedLocationClient.requestLocationUpdates(locationRequest,  new MyLocationCallback(), null);
+    }
+
+    /**
+     * 位置情報受取コールバッククラス
+     */
+    private class MyLocationCallback extends LocationCallback {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            if (locationResult == null) {
+                return;
+            }
+            // 現在値を取得
+            Location location = locationResult.getLastLocation();
+            TextView view = findViewById(R.id.testText);
+            view.setText("緯度:" + location.getLatitude() + " 経度:" + location.getLongitude());
+
+            DatabaseHelper helper = new DatabaseHelper(StackAccountActivity.this);
+            //データベース接続オブジェクトの取得
+            SQLiteDatabase db = helper.getWritableDatabase();
+            try{
+                String sqlIns = "INSERT INTO account (_id, locationName, latitude, longitude) VALUES(?,?,?,?)";
+                SQLiteStatement stmt = db.compileStatement(sqlIns);
+                stmt.bindLong(1,  1);
+                stmt.bindString(2, "LOCATION");
+                stmt.bindLong(3, (long)location.getLatitude());
+                stmt.bindLong(4, (long)location.getLongitude());
+
+
+
+            }
+            finally {
+                db.close();
+            }
+
+
+        };
+    }
+
+    /**
+     * 許可ダイアログの結果受取
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(requestCode == 2000 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // 位置情報取得開始
+            startUpdateLocation();
+        }
+    }
+
+
 }
